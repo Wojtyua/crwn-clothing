c9ecb59c46380938bfc9006bca4b9f623ebd17bb
Wojtyua <72139361+Wojtyua@users.noreply.github.com>
2022-06-13 15:32:35 +0200

    creating categories redux

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,16 +1,17 @@
-import { useContext, useState, useEffect, Fragment } from "react";
+import { useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import ProductCard from "../../components/product-card/product-card.component";
 
-import { CategoriesContext } from "../../contexts/categories.context";
+import { selectCategoriesMap } from "../../store/categories/categories.selector";
 
 import { CategoryTitle, CategoryContainer } from "./category.styles";
 //import './category.styles.jsx';
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+  const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
